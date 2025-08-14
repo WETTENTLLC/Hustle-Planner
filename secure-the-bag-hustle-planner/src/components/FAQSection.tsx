3e656360428aebc842373bff2b48a26c57592abd
WETTENTLLC <WETTENTERTAINMENTLLC@GMAIL.COM>
2025-08-13 20:14:48 -0700

    FINAL FIX: Remove all apostrophes causing ESLint errors

--- secure-the-bag-hustle-planner/src/components/FAQSection.tsx
+++ secure-the-bag-hustle-planner/src/components/FAQSection.tsx
@@ -156,7 +156,7 @@ export default function FAQSection() {
       <div className="mt-10 p-6 bg-gray-800/60 dark:bg-gray-100/60 rounded-lg">
         <h3 className="text-lg font-semibold mb-3 text-white dark:text-gray-800">Didn't find an answer?</h3>
         <p className="text-gray-300 dark:text-gray-700 mb-4">
-          Have a question that&apos;s not covered here? Send us a message and we&apos;ll get back to you.
+          Have a question that is not covered here? Send us a message and we will get back to you.
         </p>
         <div className="space-y-3">
           <div>
